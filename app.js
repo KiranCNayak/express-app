9d9745539a70a2fc5f981901b28849f5ec1340c2
Kiran C Nayak <33034799+KiranCNayak@users.noreply.github.com>
2023-09-26 13:46:11 +0530

    Handling requests from multiple inputs types

--- app.js
+++ app.js
@@ -66,8 +66,15 @@ app.get('^/old-page(.html)?', (_req, res) => {
 
 // Catch-all route, to serve custom 404 page
 // REM: W/o 404 status set explicitly using res.status(404)..., default status would've been 200, as the 404 file was found on the server (so 200)
-app.get('/*', (_req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+app.all('*', (req, res) => {
+  res.status(404);
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+  } else if (req.accepts('json')) {
+    res.json({ error: '⚠️ 404 ⚠️ — Page Not Found!' });
+  } else {
+    res.type('txt').send('⚠️ 404 ⚠️ — Page Not Found!');
+  }
 });
 
 // Our custom error handler
